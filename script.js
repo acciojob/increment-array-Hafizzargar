f21a28828441205058d5ae1a016b7ef2dadc8475
Hafizzargar <96607751+Hafizzargar@users.noreply.github.com>
2024-01-30 18:15:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-let arr = [1, 2, 3, 4, 5];
+let input = prompt("Enter values for the array, separated by commas (e.g., 1,2,3):");
+
+// Split the input string into an array using commas as the delimiter
+let arr1 = input.split(",");
+ let arr = arr1.map(element => parseInt(element));
 function incrementArray(arr) {
-  //your code here
+  //your code here  
+	for(let i=0;i<arr.length;i++){
+		arr[i]=arr[i]+1;
+	}
+	return arr;
 }
 
 alert(incrementArray(arr));
